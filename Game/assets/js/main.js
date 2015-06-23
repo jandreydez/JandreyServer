38a43c0cbf859288c76ac2c20c917ac677fe74f9
jandreydez <jandreydez@hotmail.com>
2015-06-23 16:51:15 -0300

    23-06-2015
    Funçoes 2 player feita faltando apenas arrumar quando der velha

--- Game/assets/js/main.js
+++ Game/assets/js/main.js
@@ -1,68 +1,84 @@
 
 $(function(){
-	
-	///Botões//			
-		//var addO = $('.pos').click(function(){$(this).append(btnO);});
-		//var addX = $('.pos').click(function(){$(this).append(btnX);});
-	//Gera Botao X
+	//Gera Botões X e O
 	btnX = "<div class='first'></div><div class='second'></div>";
- 	//Gera Botao O
- 	btnO = "<div class='circle'></div>"; 	
+ 	btnO = "<div class='circle'></div>";
+ 	//contadores
+ 	countX = 1;	
+ 	//Vetor tabuleiro
  	vet = [];
- 	//Possiveis posições de ganhar
- 	posWin = [ 	
- 				[0,1,2],[3,4,5],[6,7,8],
- 				[0,3,6],[1,4,7],[2,5,8],
- 				[0,4,8],[6,4,2]			
- 										]; 	
- 	//Gera o Tabuleiro//
+ 	//Possíveis posições de ganho
+ 	posWin = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]; 	
+ 	//Gerador do Tabuleiro
  	table = $('.table');
- 	var i,j = 0;	
+ 	j=0;	
 	for(i=0;i<9;i++){
 		vet[i]= i;				
 		if(j == i){
 	  		table.append("<br style='clear:both'/>");
 	  		j+=3;
-	  	}	  	
-	  	table.append("<div class='pos "+vet[i]+"'>"+vet[i]+"</div>");		  	 	
+	  	}
+	  	table.append("<div class='pos'></div>");		  	 	
 	}
+	//Checa vencedor
+	function CheckWin(){
+		for(i=0;i<posWin.length;i++){
+			x = 0; o = 0
+			
+			for(j=0;j<posWin[i].length;j++){
+				if($('.pos').eq(posWin[i][j]).hasClass('x'))x+=1;
+					if(x==posWin[i].length){
+						$('.result').show().html('<h2>X GANHOU</h2>');
+						$('.countX').html("").append(countX++);
+					}
+				else if($('.pos').eq(posWin[i][j]).hasClass('o'))o+=1;
+				 		if(o==posWin[i].length)$('.result').show().html('<h2>O GANHOU</h2>');
+				
+			}	
+		}
+	}	
 	//Função Checa Jogador//
-	function CheckPlayer(){		
+	function CheckPlayer(){	
 			$('.pos').click(function(){
 				if($('.p1').hasClass('active')) {
-					if($(this).hasClass('x')||$(this).hasClass('o')){				
-						
-					}
-					else{$(this).addClass('x').append(btnX);
+					if($(this).hasClass('x')||$(this).hasClass('o')){}
+					else{
+						$(this).addClass('x').append(btnX);
 						$('.p1').removeClass('active');
 						$('.p2').addClass('active');
+						CheckWin();		
 					}
 				}
-				else{
-					if($(this).hasClass('x')||$(this).hasClass('o')){				
-						
-					}
-					else{$(this).addClass('x').append(btnO);
+				else if($(this).hasClass('x')||$(this).hasClass('o')){
+
+				}				
+					else{$(this).addClass('o').append(btnO);
 						$('.p2').removeClass('active');
 						$('.p1').addClass('active');
-					}
-				}
-						
-			});
-				
+						CheckWin();						
+					}										
+			});				
 	}
 	//Reinicia o jogo
 	function RestartGame(){
 		$('.restart').click(function(){
-			$('.pos').removeClass('x');
+			$('.pos').removeClass('x o').html("");
+			$('.result').hide("fade");
 		});
-	}
-	//////////////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////////////
-	//CHAMA FUNÇÔES//
+	}	
 	RestartGame();
 	CheckPlayer();
-	//////////////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////////////
+	//*Painel*//
+	//Desktop
+	{$(".opcoes").click(function(){
+		$('.painelop').slideToggle();
+		$('.opcoes').html("Ok");
+		changecolor();
+	});}
+	//Troca cor do Tabuleiro
+	function changecolor(){
+	MyColor = $('#color').val();
+	$('.pos').css("background-color",MyColor);
+	}
+
 });
--- Game/game.php
+++ Game/game.php
@@ -11,24 +11,15 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <!--<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>-->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="assets/vendor/js/jquery-1.11.3.min.js"></script>
     <script src="assets/js/main.js"></script>
   </head>
-  <body><h2 class="p1 active">Jogador 1 
-        <div class="pos bgnone">          
-            <div class="first"></div>
-            <div class="second"></div>          
-        </div>
-          <h2 class="p2">Jogador 2 
-        <div class="pos bgnone">          
-            <div class="circle"></div>         
-        </div>
-      </h2>
+  <body>
     <div class="container">
-      <div class="row">
-
+      <div class="row relative">
         <!--<div class="pos">
           <div class="btn-x">
             <div class="first"></div>
@@ -40,24 +31,33 @@
             <div class="circle"></div>
           </div>
         </div>-->
-        <div class="col-md-3">          
-      <div class="restart">Restart Game</div>
-      <div class="table"></div>
-<!--------------Gera o Tabuleiro------------------>
-<?php //Table//
-  /*for($i=0;$i<3;$i++){echo"<br style='clear:both'/>";
-    for($j=0;$j<3;$j++){
-      echo"<div class='".$i.$j." pos'>".$i.$j."</div>";
-    }
-  }*/
-?>
-<!------------------------------------------------>
-          
-        </div>
-      </div>
-    </div>
+        
+            <div class="col-md-3 col-md-offset-3 col-sm-4">
+                <h2 class="p1 active">Player1 X</h2>
+                <h2 class="p2">Player2 O</h2>    
+                <div class="table">
+                  <div class="result"></div></div>
+                <div></div>          
+            </div>
+            <div class="col-md-4">
+                <div class="button restart">Restart Game</div>
+                <ul class="painelop">
+                  <li>
+                    <h4>Selecione a cor do Tabuleiro</h4><input type="color" id="color" value='#639639' name="color"></li></ul>
+                <div class="button opcoes">Options</div>
+                <ul class="count">
+                  <li class="countX">0</li>
+                  <li>0</li>
+                  <li>0</li>
+                </ul>
+
+                
+            </div>        
+              
+        
+  
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>    
-    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->    
+    
   </body>
 </html>
